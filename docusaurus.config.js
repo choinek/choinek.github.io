0b9ae113237506b498524aa5cd33ff6599c60420
Adrian Chojnicki <adrian@chojnicki.pl>
2025-01-13 00:03:37 +0100

    + test dinosaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-  title: 'Enhanced Docusaurus Site',
-  tagline: 'Static Site Generator with Extra Features',
-  url: 'https://choinek.github.io',
-  baseUrl: '/choinek.github.io/',
-  organizationName: 'choinek', // GitHub username or organization
-  projectName: 'choinek.github.io', // Repository name
-  deploymentBranch: 'gh-pages', // Deployment branch
-  plugins: [
-    [
-      require.resolve('docusaurus-lunr-search'),
-      {
-        languages: ['en'],
-      },
+    title: 'Choinek Dinosaurus Test',
+    tagline: 'Static Site Generator with Extra Features',
+    url: 'https://choinek.github.io',
+    baseUrl: '/choinek.github.io/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    organizationName: 'choinek', // GitHub username or organization
+    projectName: 'choinek.github.io', // Repository name
+    deploymentBranch: 'gh-pages', // Deployment branch
+    plugins: [
+        [
+            'docusaurus-lunr-search',
+            {
+                languages: ['en'], // Add more languages if needed
+            },
+        ],
     ],
-  ],
-  themeConfig: {
-    colorMode: {
-      defaultMode: 'dark',
-      respectPrefersColorScheme: true,
+    themeConfig: {
+        colorMode: {
+            defaultMode: 'dark',
+            respectPrefersColorScheme: true,
+        },
     },
-  },
 };
