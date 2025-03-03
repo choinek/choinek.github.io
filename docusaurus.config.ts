7e403467bf97637306faa3713177791639eb3714
Adrian Chojnicki <adrian@chojnicki.pl>
2025-03-03 02:10:57 +0100

    + deploy cleanups

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,12 +12,12 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+  trailingSlash: true,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'choinek  ', // Usually your GitHub org/user name.
+  organizationName: 'choinek', // Usually your GitHub org/user name.
   projectName: 'choinek.github.io', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'log',
   onBrokenMarkdownLinks: 'log',
 
