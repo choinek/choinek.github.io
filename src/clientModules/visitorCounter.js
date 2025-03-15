cfbf7f0acc1638f0fd3d01fd48017eb917bf5f21
Adrian Chojnicki <adrian@chojnicki.pl>
2025-03-15 11:16:19 +0100

    visitor counter wip

---
+++ src/clientModules/visitorCounter.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+// import { incrementVisitorCount } from "../utils/visitorCounter";
+
+export default function VisitorCount() {
+  useEffect(() => {
+    // incrementVisitorCount();
+  }, []);
+
+  return null;
+}
