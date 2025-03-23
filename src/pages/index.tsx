8b1ed81f55697d2a4fd051497b50a43c3c7538be
Adrian Chojnicki <adrian@chojnicki.pl>
2025-03-23 18:28:17 +0100

    + fix link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/adrians-tutorials/git/rebase">
+            to="/docs/guide/git/rebase/">
             Git rebase tutorial
           </Link>
         </div>
